45e7fb38fe079ee61eeccd86bb0cf8dc61451c9c
Marc <marc.ferraro1@gmail.com>
2021-03-10 09:59:35 -0600

    Navbar Wired

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar'
 
 class App extends Component {
 
@@ -12,8 +14,11 @@ class App extends Component {
 
   render() {
     return(
-    <div >
-      hello world
+    <div className="App" >
+      <Navbar />
+      <Switch>
+        <Route />
+      </Switch>
     </div>
     )};
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link }  from 'react-router-dom'
+
+class Navbar extends React.Component {
+    render(){
+        return(
+            <div>Hello from navbar</div>
+        )
+    }
+}
+
+export default Navbar
