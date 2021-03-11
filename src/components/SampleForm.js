8fff8aba24525af56b46998cc308304490f28ac4
Marc <marc.ferraro1@gmail.com>
2021-03-11 12:22:18 -0600

    Checkbox added

--- src/components/SampleForm.js
+++ src/components/SampleForm.js
@@ -1,10 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
+
 
 function SampleForm(props){
+    const [title, setTitle] = useState("")
+    const [note, setNote] = useState("")
+    const [date, setDate] = useState("")
+    const [shared, setShared] = useState(false)
+    const classes = useStyles();
+
+    const handleSubmit = () =>{
+
+    }
+
     return(
-        <div>hello from sample for,</div>
+        <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
+                <TextField onChange={(event) => setTitle(event.target.value)} value={title} id="outlined-basic" label="Title" variant="outlined" />
+                <TextField onChange={(event) => setTitle(event.target.value)} value={note} id="outlined-basic" label="Note" variant="outlined" />
+                <TextField onChange={(event) => setTitle(event.target.value)} value={date} id="outlined-basic" label="Date" variant="outlined" />
+                <FormGroup>
+                    <FormControlLabel
+                        control={<Checkbox checked={shared} onChange={() => setShared(!shared)} name="share-sample" />}
+                        label="Share Sample?"
+                    />
+                </FormGroup>
+                <Button type="submit" variant="contained" color="secondary"endIcon={<ArrowForwardIosIcon />}>Submit</Button>
+        </form>
     )
 }
 
-export default SampleForm
+const mapStateToProps = state => {
+    return{
+        user: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(SampleForm)
