a8af1954ce18f6bc4fa601c9a5d46b6846cf83eb
Marc <marc.ferraro1@gmail.com>
2021-05-26 16:24:03 -0500

    useEffect imported

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Login from './components/Login'
