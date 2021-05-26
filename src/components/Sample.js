66ebb93dc8de95a877e60dc42211e763fdc94e9a
Marc <marc.ferraro1@gmail.com>
2021-05-26 17:39:53 -0500

    pause imported

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -7,6 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import { connect } from 'react-redux';
 import Box from '@material-ui/core/Box';
@@ -67,7 +68,6 @@ function Sample(props){
       }
     }
     
-
     return(
       <Box display="flex" justifyContent="center">
         <Card className={cardClasses.root}>
