0f49974fec2d92ec67d20fbe42a190182d78604c
Marc <marc.ferraro1@gmail.com>
2021-05-26 16:23:36 -0500

    useState imported for app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Login from './components/Login'
