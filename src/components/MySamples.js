1d83515058cf705d521dfde9ed4dd88d0c08b7b3
Marc <marc.ferraro1@gmail.com>
2021-07-05 23:50:34 -0500

    my samples component added

---
+++ src/components/MySamples.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SampleCard from './SampleCard';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+
+const gridStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        height: 140,
+        width: 100,
+    },
+    control: {
+        padding: theme.spacing(2),
+    },
+}));
+
+
+function SampleContainer(props){
+    const gridClasses = gridStyles();
+    
+    return(
+        <Grid container className={gridClasses.root} spacing={2}>
+            {/* <Grid item xs={12}>
+                <Grid container justify="center" spacing={2}>
+                    {props.samples.map(sample => (
+                        <Grid key={sample.id} item>
+                            <SampleCard key={sample.id} sample={sample} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid> */}
+        </Grid>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        samples: state.samples
+    }
+}
+// const mapDispatchToProps = {
+
+// }
+
+export default connect(mapStateToProps)(SampleContainer)
